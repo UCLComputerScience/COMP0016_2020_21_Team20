b6e182e3fd4ca4f236fd64a5ab4b76f3fb120acb
Shubham Jain <shubham@sjain.dev>
2020-11-24 20:08:14 +0000

    Add initial self-assessment page
    Co-authored-by: Matthieu Schulz <Matthew.schulz.19@ucl.ac.uk>
    Co-authored-by: Zielinski <zcabmzi@ucl.ac.uk>

---
+++ pages/self-assessment.js
@@ -0,0 +1,62 @@
+import { Question } from '../components';
+
+import { Button } from '@material-ui/core';
+//import { Icon } from '@material-ui/icons';
+//import SaveIcon from '@material-ui/icons/SaveIcon';
+
+import styles from './self-assessment.module.css';
+
+const questions = [
+  {
+    question:
+      'I am confident/reassured that I have screened for serious pathology to an appropriate level in this case',
+    questionId: 1,
+    url: 'https://example.com',
+  },
+  {
+    question:
+      'I have listened to and understood the patient’s knowledge and concerns about their presentation.',
+    questionId: 2,
+    url: 'https://example.com',
+  },
+  {
+    question:
+      'I have applied knowledge of best evidence to the context of this patient’s presentation to present appropriate treatment options to the patient.',
+    questionId: 3,
+    url: 'https://example.com',
+  },
+];
+
+function selfAssessment() {
+  return (
+    <div>
+      <h1>Your Self Assessement</h1>
+      <h3>
+        To what extent do you agree with the following statements regarding your
+        experience in the last week?
+      </h3>
+      <div className={styles.selfAssessmentContainer}>
+        {questions.map((question, i) => (
+          <Question
+            key={i}
+            question={question.question}
+            questionId={question.questionId}
+            questionNumber={i + 1}
+            questionUrl={question.url}
+          />
+        ))}
+
+        <Button
+          variant="contained"
+          color="primary"
+          className={styles.submit}
+          // startIcon={<SaveIcon />}
+        >
+          Submit
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default selfAssessment;
