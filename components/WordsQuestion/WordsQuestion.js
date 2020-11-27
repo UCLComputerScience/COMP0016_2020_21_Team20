6b05ab1a52207604ac00bd1d4f5a1c7854790f56
Shubham Jain <shubham@sjain.dev>
2020-11-27 14:55:02 +0000

    Finish WordsQuestion component; Pass state up to self-assessment
    Co-authored-by: Matthieu Schulz <Matthew.schulz.19@ucl.ac.uk>
    Co-authored-by: Zielinski <zcabmzi@ucl.ac.uk>

--- components/WordsQuestion/WordsQuestion.js
+++ components/WordsQuestion/WordsQuestion.js
@@ -6,20 +6,37 @@ import styles from './WordsQuestion.module.css';
 
 const MAX_NUMBER_OF_WORDS = 3;
 
-function generateInputs() {
-  const inputs = [];
-  for (let i = 0; i < MAX_NUMBER_OF_WORDS; i++) {
-    inputs.push(<Input />);
-  }
-  return inputs;
-}
-
 function WordsQuestion(props) {
-  const [words, setWords] = useState([]);
+  const [word1, setWord1] = useState(null);
+  const [word2, setWord2] = useState(null);
+  const [word3, setWord3] = useState(null);
 
-  const updateWords = word => {
-    setWords(words.concat(word));
-    props.onChange(words);
+  const generateInputs = () => {
+    const inputs = [];
+    for (let i = 0; i < MAX_NUMBER_OF_WORDS; i++) {
+      let setter;
+      if (i === 0) {
+        setter = setWord1;
+      } else if (i === 1) {
+        setter = setWord2;
+      } else if (i === 2) {
+        setter = setWord3;
+      }
+
+      inputs.push(
+        <Input
+          className={styles.input}
+          key={i}
+          onChange={event => setWord(setter, event.target.value)}
+        />
+      );
+    }
+    return inputs;
+  };
+
+  const setWord = (setter, word) => {
+    setter(word);
+    props.onChange([word1, word2, word3]);
   };
 
   return (
@@ -27,7 +44,7 @@ function WordsQuestion(props) {
       <div className={styles.questionText}>
         {props.questionNumber}. {props.question}
       </div>
-      {generateInputs()}
+      <div className={styles.wordsInputContainer}>{generateInputs()}</div>
     </div>
   );
 }
