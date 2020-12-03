15749452e11216a9b46a15e9cd8f30d7e76cad29
Shubham Jain <shubham@sjain.dev>
2020-12-03 22:36:21 +0000

    Implement /api/responses backend
    This uses the Prisma Client to interact with the database for reads &
    writes.
    
    Co-authored-by: Matthieu Schulz <Matthew.schulz.19@ucl.ac.uk>
    Co-authored-by: Zielinski <zcabmzi@ucl.ac.uk>

---
+++ pages/api/responses.js
@@ -0,0 +1,40 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    return res.json(await prisma.responses.findMany());
+  }
+
+  if (req.method === 'POST') {
+    const scores = req.body.scores.map(scoreObj => {
+      return {
+        score: scoreObj.score,
+        standards: { connect: { id: scoreObj.standardId } },
+      };
+    });
+
+    const words = req.body.good_words.map(word => {
+      return {
+        word: word,
+        questions: { connect: word.questionId },
+      };
+    });
+
+    const insertion = await prisma.responses.create({
+      data: {
+        timestamp: new Date(),
+        departments: { connect: { id: req.body.dept_id } },
+        users: { connect: { id: req.body.user_id } },
+        is_mentoring_session: req.body.is_mentoring_session,
+        scores: { create: scores },
+        words: { create: words },
+      },
+    });
+
+    return res.json(insertion);
+  }
+
+  res.statusCode = 405;
+  res.end('HTTP Method Not Allowed');
+}
