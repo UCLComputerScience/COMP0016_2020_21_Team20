01ab11ca5025e1a28ec0c629e612ce16ce2ef459
Shubham Jain <shubham@sjain.dev>
2020-12-06 15:29:48 +0000

    Add filtering support to GET responses backend

--- pages/api/responses.js
+++ pages/api/responses.js
@@ -2,29 +2,45 @@ import prisma from '../../lib/prisma';
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
-    return res.json(await prisma.responses.findMany());
+    const { from, to, dept_id: deptId, user_id: userId } = req.query;
+
+    const filters = [];
+    if (from) filters.push({ timestamp: { gte: new Date(from) } });
+    if (to) filters.push({ timestamp: { lte: new Date(to) } });
+    if (userId) filters.push({ user_id: { equals: userId } });
+    if (deptId) filters.push({ dept_id: { equals: deptId } });
+
+    if (filters.length) {
+      return res.json(
+        await prisma.responses.findMany({
+          where: { AND: filters },
+        })
+      );
+    } else {
+      return res.json(await prisma.responses.findMany());
+    }
   }
 
   if (req.method === 'POST') {
     const scores = req.body.scores.map(scoreObj => {
       return {
-        score: scoreObj.score,
         standards: { connect: { id: scoreObj.standardId } },
+        score: scoreObj.score,
       };
     });
 
-    const words = req.body.good_words.map(word => {
+    const words = req.body.words.map(word => {
       return {
-        word: word,
         questions: { connect: word.questionId },
+        word: word,
       };
     });
 
     const insertion = await prisma.responses.create({
       data: {
+        users: { connect: { id: req.body.user_id } },
         timestamp: new Date(),
         departments: { connect: { id: req.body.dept_id } },
-        users: { connect: { id: req.body.user_id } },
         is_mentoring_session: req.body.is_mentoring_session,
         scores: { create: scores },
         words: { create: words },
