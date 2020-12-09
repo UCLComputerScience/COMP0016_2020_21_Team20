06876701f29f6cc49edd96259c38b28ba24529c5
Shubham Jain <shubham@sjain.dev>
2020-12-09 22:31:35 +0000

    Update docker-compose.yml to add Keycloak container & new database
    The PostgreSQL container now contains 2 databases: keycloak and
    nhsw_self_assessment. Both will be owned by the same user.
    
    A helper script is used to create the multiple databases on first run.

---
+++ create-multiple-postgresql-databases.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script tweaked from https://github.com/mrts/docker-postgresql-multiple-databases
+set -e
+set -u
+
+function create_database() {
+	local database=$1
+	echo "  Creating database '$database'"
+	PGPASSWORD=$POSTGRES_PASSWORD psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+	    CREATE DATABASE $database;
+	    GRANT ALL PRIVILEGES ON DATABASE $database TO "$POSTGRES_USER";
+EOSQL
+}
+
+if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
+	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
+	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
+		create_database $db
+	done
+	echo "Multiple databases created"
+fi
