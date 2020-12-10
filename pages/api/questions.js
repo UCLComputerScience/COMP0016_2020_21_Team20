5844a2831657ef02ccebbb8e8896f000992a2447
Shubham Jain <shubham@sjain.dev>
2020-12-10 21:01:58 +0000

    Add authentication check to API endpoints

--- pages/api/questions.js
+++ pages/api/questions.js
@@ -1,6 +1,16 @@
 import prisma from '../../lib/prisma';
 
+import { getSession } from 'next-auth/client';
+
 export default async function handler(req, res) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    res.status = 401;
+    res.end('Unauthorized access');
+    return;
+  }
+
   if (req.method !== 'GET') {
     res.statusCode = 405;
     res.end('HTTP Method Not Allowed');
