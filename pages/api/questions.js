14d17e7c9809eedcb5c02ed40f8e7ee4ae1efc59
Shubham Jain <shubham@sjain.dev>
2020-12-14 18:14:11 +0000

    Implement POST /api/questions for admins to create new questions

--- pages/api/questions.js
+++ pages/api/questions.js
@@ -1,4 +1,5 @@
 import prisma from '../../lib/prisma';
+import roles from '../../lib/roles';
 
 import { getSession } from 'next-auth/client';
 
@@ -7,28 +8,50 @@ export default async function handler(req, res) {
 
   if (!session) {
     res.status = 401;
-    res.end('Unauthorized access');
-    return;
+    return res.end('Unauthorized access');
   }
 
-  if (req.method !== 'GET') {
-    res.statusCode = 405;
-    res.end('HTTP Method Not Allowed');
-    return;
-  }
-
-  const questions = await prisma.questions.findMany();
-
-  // Return an object with keys as question types, and values as arrays of questions with each type
-  // e.g. { likert_scale: [{...}, {...}], words: [{...}, {...}] }
-  const questionsToReturn = questions.reduce((result, question) => {
-    if (result[question.question_type]) {
-      result[question.question_type].push(question);
-    } else {
-      result[question.question_type] = [question];
+  if (req.method === 'POST') {
+    if (!session.roles.includes(roles.USER_TYPE_ADMIN)) {
+      res.statusCode = 403;
+      return res.end('You do not have permission to add new questions');
     }
-    return result;
-  }, {});
 
-  return res.json(questionsToReturn);
+    const { body, url, standard, type } = req.body;
+    if (!body || !url || !standard || !type) {
+      res.statusCode = 422;
+      return res.end('The required question details are missing');
+    }
+
+    const record = await prisma.questions.create({
+      data: {
+        question_body: body,
+        question_url: url,
+        standards: { connect: { id: standard } },
+        question_type: type,
+      },
+    });
+
+    return res.json(record);
+  }
+
+  if (req.method === 'GET') {
+    const questions = await prisma.questions.findMany();
+
+    // Return an object with keys as question types, and values as arrays of questions with each type
+    // e.g. { likert_scale: [{...}, {...}], words: [{...}, {...}] }
+    const questionsToReturn = questions.reduce((result, question) => {
+      if (result[question.question_type]) {
+        result[question.question_type].push(question);
+      } else {
+        result[question.question_type] = [question];
+      }
+      return result;
+    }, {});
+
+    return res.json(questionsToReturn);
+  }
+
+  res.statusCode = 405;
+  return res.end('HTTP Method Not Allowed');
 }
