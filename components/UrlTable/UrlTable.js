731fed4054450eac19a7651619f5409ac6755eff
Zielinski <zcabmzi@ucl.ac.uk>
2020-12-21 20:17:29 +0000

    update manage urls to be ready for api and update manage security

--- components/UrlTable/UrlTable.js
+++ components/UrlTable/UrlTable.js
@@ -8,11 +8,14 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import { Button, Input } from '@material-ui/core';
+import CreateIcon from '@material-ui/icons/Create';
+import ClearIcon from '@material-ui/icons/Clear';
+import SaveIcon from '@material-ui/icons/Save';
 
-import styles from './Table.module.css';
+import styles from './UrlTable.module.css';
 
 const columns = [
-  { id: 'question', label: 'Question', minWidth: 50 },
+  { id: 'question', label: 'Question body', minWidth: 50 },
   { id: 'standard', label: 'Standard', minWidth: 50 },
   {
     id: 'url',
@@ -24,7 +27,7 @@ const columns = [
       </a>
     ),
   },
-  { id: 'edit', label: '', minWidth: 50 },
+  { id: 'actions', label: 'Actions', minWidth: 50 },
 ];
 
 const useStyles = makeStyles({
@@ -41,29 +44,50 @@ var rows = [
   {
     question:
       'I am confident/reassured that I have screened for serious pathology to an appropriate level in this case.',
-    standard: 'Timely care',
+    standard: 'Staff and Resources',
     url: 'https://example.com',
-    edit: null,
   },
   {
     question:
       'I have applied knowledge of best evidence to the context of this patient’s presentation to present appropriate treatment options to the patient.',
-    standard: 'Safe care',
+    standard: 'Staying Healthy',
     url: 'https://example2.com',
-    edit: null,
   },
   {
     question:
       'I have optimised the opportunity in our interaction today to discuss relevant activities and behaviours that support wellbeing and a healthy lifestyle for this patient.',
-    standard: 'Dignified care',
+    standard: 'Individual Care',
     url: 'https://example3.com',
-    edit: null,
+  },
+  {
+    question:
+      'I have listened and responded with empathy to the patient’s concerns.',
+    standard: 'Timely Care',
+    url: 'https://example4.com',
+  },
+  {
+    question:
+      'I have supported the patient with a shared decision making process to enable us to agree a management approach that is informed by what matters to them.',
+    standard: 'Dignified Care',
+    url: 'https://example5.com',
+  },
+  {
+    question:
+      'I have established progress markers to help me and the patient monitor and evaluate the success of the treatment plan.',
+    standard: 'Effective Care',
+    url: 'https://example6.com',
+  },
+  {
+    question:
+      'My reflection/discussion about this interaction has supported my development through consolidation or a unique experience I can learn from.',
+    standard: 'Safe Care',
+    url: 'https://example7.com',
   },
 ];
 
 export default function StickyHeadTable() {
   const classes = useStyles();
-  const [editing, setEditing] = useState(false);
+  const [editing, setEditing] = useState(null);
   var newData = rows.map(row => ({
     question: row.question,
     standard: row.standard,
@@ -71,7 +95,7 @@ export default function StickyHeadTable() {
     edit: row.edit,
   }));
 
-  const setToCurrentData = () => {
+  const setToDatabaseData = () => {
     newData = rows.map(row => ({
       question: row.question,
       standard: row.standard,
@@ -80,40 +104,19 @@ export default function StickyHeadTable() {
     }));
   };
 
-  const handleEdit = () => {
-    setEditing(!editing);
-    setToCurrentData;
+  const cancelEditing = () => {
+    setEditing(false);
+    setToDatabaseData;
   };
 
   const sendData = () => {
     //will need to send data to db here, then set this data in local copy for table
-    rows = newData.map(row => ({
-      question: row.question,
-      standard: row.standard,
-      url: row.url,
-      edit: row.edit,
-    }));
-    setEditing(false);
+    rows[editing].url = newData[editing].url
+    setEditing(null);
   };
 
   return (
     <div>
-      <div className={styles.buttons}>
-        <Button
-          variant="contained"
-          color={editing ? 'secondary' : 'primary'}
-          onClick={() => handleEdit()}>
-          {editing ? 'Discard changes' : "Edit URL's"}
-        </Button>{' '}
-        {editing && (
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => sendData()}>
-            Save changes
-          </Button>
-        )}
-      </div>
       <Paper className={classes.root}>
         <TableContainer className={classes.container}>
           <Table stickyHeader aria-label="sticky table">
@@ -124,9 +127,9 @@ export default function StickyHeadTable() {
                     key={column.id}
                     align={column.align}
                     style={{ minWidth: column.minWidth }}>
-                    {column.id === 'edit' && editing
-                      ? 'New Training URL'
-                      : column.label}
+                    <div className={styles.header}>
+                      {column.label}
+                    </div>
                   </TableCell>
                 ))}
               </TableRow>
@@ -139,9 +142,7 @@ export default function StickyHeadTable() {
                       const value = row[column.id];
                       return (
                         <TableCell key={column.id} align={column.align}>
-                          {column.id === 'url' ? (
-                            column.format(value)
-                          ) : column.id === 'edit' && editing ? (
+                          {column.id === 'url' && editing === i ? (
                             <Input
                               className={styles.input}
                               key={row['standard']}
@@ -151,9 +152,34 @@ export default function StickyHeadTable() {
                                 (newData[i].url = event.target.value)
                               }
                             />
+                          ) : column.id === 'url' ? (
+                            column.format(value)
+                          ) : column.id === 'actions' && editing === i ? (
+                            <div>
+                              <Button
+                                variant="contained"
+                                color="primary"
+                                onClick={() => sendData()}>
+                                <SaveIcon fontSize="inherit" />
+                              </Button>
+                              {' '}
+                              <Button
+                                variant="contained"
+                                color="secondary"
+                                onClick={() => cancelEditing()}>
+                                <ClearIcon fontSize="inherit" />
+                              </Button>
+                            </div>
+                          ) : column.id === 'actions' ? (
+                            <Button
+                              variant="contained"
+                              color="primary"
+                              onClick={() => setEditing(i)}>
+                              <CreateIcon fontSize="inherit" />
+                            </Button>
                           ) : (
-                            value
-                          )}
+                                    value
+                                  )}
                         </TableCell>
                       );
                     })}
