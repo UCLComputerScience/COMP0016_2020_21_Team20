35ca9fdcd5f3833d456545d0a8b15cd169cc0c63
Shubham Jain <shubham@sjain.dev>
2020-12-26 18:46:29 +0000

    Simplify join_codes API by combining clinician and department endpoints

---
+++ pages/api/join_codes/[...params].js
@@ -0,0 +1,59 @@
+import { getSession } from 'next-auth/client';
+
+import prisma from '../../../lib/prisma';
+import roles from '../../../lib/roles';
+
+export default async function handler(req, res) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    res.status = 401;
+    res.end('Unauthorized access');
+    return;
+  }
+
+  const { params } = req.query;
+  const [type, departmentId] = params;
+
+  if (
+    ![roles.USER_TYPE_DEPARTMENT, roles.USER_TYPE_HOSPITAL].includes(type) ||
+    !departmentId
+  ) {
+    res.statusCode = 404;
+    return res.end('Route not found');
+  }
+
+  if (req.method === 'PUT') {
+    if (!session.roles.includes(type)) {
+      res.statusCode = 403;
+      return res.end(
+        `You do not have permission to modify
+         ${
+           type === roles.USER_TYPE_DEPARTMENT
+             ? 'department-level'
+             : 'clinician-level'
+         } join codes`
+      );
+    }
+
+    const code = await createJoinCode();
+    const dbTable =
+      type === roles.USER_TYPE_DEPARTMENT
+        ? prisma.department_join_codes
+        : prisma.clinician_join_codes;
+
+    await dbTable.upsert({
+      create: {
+        departments: { connect: departmentId },
+        code,
+      },
+      update: { code },
+      where: { department_id: departmentId },
+    });
+
+    return res.json({ code });
+  }
+
+  res.statusCode = 405;
+  res.end('HTTP Method Not Allowed');
+}
