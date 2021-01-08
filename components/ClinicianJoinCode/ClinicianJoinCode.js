c8520b560ddc15def4f6a3dd63ae98c1c1b1804b
Zielinski <zcabmzi@ucl.ac.uk>
2021-01-08 11:06:58 +0000

    fixed getting codes

--- components/ClinicianJoinCode/ClinicianJoinCode.js
+++ components/ClinicianJoinCode/ClinicianJoinCode.js
@@ -7,6 +7,7 @@ import styles from './ClinicianJoinCode.module.css';
 import useSWR from '../../lib/swr';
 import { mutate } from 'swr';
 import { useSession } from 'next-auth/client';
+import roles from '../../lib/roles';
 
 const getCode = (id) => {
     const { data, error } = useSWR('/api/departments/' + id, {
@@ -19,33 +20,49 @@ const getCode = (id) => {
 
 function ClinicianJoinCode() {
     const [session] = useSession();
-    console.log(session.user.departmentId);
+    //console.log(session.user.departmentId);
     const code = getCode(session.user.departmentId);
     //console.log(code);
 
+    const regenerateInDatabase = async id => {
+        const res = await fetch('/api/join_codes/' + roles.USER_TYPE_DEPARTMENT + '/' + id, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+        });
+        return await res.json();
+    };
+
+    const regenerateCode = async id => {
+        await regenerateInDatabase(id)
+        mutate('/api/departments/' + id);
+    };
+
     return (
         <div className={styles.content}>
-            <div>
-                Please send this unique URL to clinicians so they can join your department:
+            <div className={styles.textPadding}>
+                {"Please send this unique URL to clinicians so they can join your "
+                    + (code !== undefined ? code["0"]["name"] : 'loading...') + " department:"}
             </div>
-            <div>
-                {window.location.hostname + '/join/clincian/' + code}
+            <div className={styles.textPadding}>
+                {window.location.hostname + '/join/clincian/' + (code !== undefined ? code["0"]["clinician_join_codes"]["code"] : 'loading...')}
             </div>
-            <div className={styles.button}>
-                <CopyToClipboard
-                    text={window.location.hostname + '/join/clincian/' + code}>
-                    <button><FileCopyIcon fontSize="inherit" /></button>
-                </CopyToClipboard>
-            </div>
-            <div>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={() => console.log(window.location.hostname + '/join/clincian/' + code)}>
-                    <div className={styles.buttonText}>
-                        Re-generate URL
+            <div className={styles.buttonPadding}>
+                <div className={styles.button}>
+                    <CopyToClipboard
+                        text={window.location.hostname + '/join/clincian/' + (code !== undefined ? code["0"]["clinician_join_codes"]["code"] : 'loading...')}>
+                        <button><FileCopyIcon fontSize="inherit" /></button>
+                    </CopyToClipboard>
+                </div>
+                <div>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => regenerateCode(session.user.departmentId)}>
+                        <div className={styles.buttonText}>
+                            Re-generate URL
                     </div>
-                </Button>
+                    </Button>
+                </div>
             </div>
         </div>
     )
