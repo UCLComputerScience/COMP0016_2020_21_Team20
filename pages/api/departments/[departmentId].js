e9c69873117bc58ef461f1d839c43fba33babc57
Shubham Jain <shubham@sjain.dev>
2021-01-16 21:43:23 +0000

    Add DELETE /api/departments/ID endpoint
    This archives the department in our database.

--- pages/api/departments/[departmentId].js
+++ pages/api/departments/[departmentId].js
@@ -2,36 +2,63 @@ import prisma from '../../../lib/prisma';
 import roles from '../../../lib/roles';
 
 import { getSession } from 'next-auth/client';
+import { user_type } from '@prisma/client';
 
 export default async function handler(req, res) {
-    const session = await getSession({ req });
+  const session = await getSession({ req });
 
-    if (!session) {
-        res.status = 401;
-        res.end('Unauthorized access');
-        return;
+  if (!session) {
+    res.status = 401;
+    res.end('Unauthorized access');
+    return;
+  }
+
+  if (req.method === 'GET') {
+    if (!session.roles.includes(roles.USER_TYPE_DEPARTMENT)) {
+      res.statusCode = 403;
+      return res.end(
+        'You do not have permission to view individual departments'
+      );
     }
 
-    if (req.method === 'GET') {
-        if (!session.roles.includes(roles.USER_TYPE_DEPARTMENT)) {
-            res.statusCode = 403;
-            return res.end('You do not have permission to view individual departments');
-        }
+    const includes = {};
 
-        const includes = {};
-
-        if (session.roles.includes(roles.USER_TYPE_DEPARTMENT)) {
-            includes.clinician_join_codes = { select: { code: true } };
-        }
-
-        const department = await prisma.departments.findMany({
-            where: { id: +req.query.departmentId },
-            include: includes,
-        });
-
-        return res.json(department);
+    if (session.roles.includes(roles.USER_TYPE_DEPARTMENT)) {
+      includes.clinician_join_codes = { select: { code: true } };
     }
 
-    res.statusCode = 405;
-    res.end('HTTP Method Not Allowed');
+    const department = await prisma.departments.findMany({
+      where: { id: +req.query.departmentId },
+      include: includes,
+    });
+
+    return res.json(department);
+  }
+
+  if (req.method === 'DELETE') {
+    if (!session.roles.includes(roles.USER_TYPE_HOSPITAL)) {
+      res.statusCode = 403;
+      return res.end('You do not have permission to delete departments');
+    }
+
+    if (session.user.departmentId !== +req.query.departmentId) {
+      res.statusCode = 403;
+      return res.end('You do not have permission to delete this department');
+    }
+
+    const responses = await Promise.all([
+      prisma.departments.update({
+        data: { archived: true },
+        where: { id: +req.query.departmentId },
+      }),
+      prisma.question_urls.deleteMany({
+        where: { department_id: { equals: +req.query.departmentId } },
+      }),
+    ]);
+
+    return res.json({ success: responses.every(r => !!r) });
+  }
+
+  res.statusCode = 405;
+  res.end('HTTP Method Not Allowed');
 }
