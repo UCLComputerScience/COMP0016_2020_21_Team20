36359f93eb41c8ef48dfd342a44848be73587b5f
Matthieu Schulz <m.schulz18@ejm.org>
2021-01-21 12:50:05 +0000

    isMentoringSession color on graph finished

--- components/LineChart/LineChart.js
+++ components/LineChart/LineChart.js
@@ -8,8 +8,6 @@ const baseProperties = {
   fill: false,
   lineTension: 0.1,
   pointBackgroundColor: '#fff',
-  pointBorderWidth: 5,
-  label: '',
   // data, label, borderColor remaining
 };
 
@@ -20,27 +18,31 @@ const formatData = data => {
     datasets: [],
   };
 
-  console.log('test', data[0]);
-  if (data[0].is_mentoring_session === true) {
-    baseProperties.pointBackgroundColor = 'red';
-    baseProperties.pointBorderWidth = 10;
-  } else {
-    baseProperties.pointBackgroundColor = '#fff';
-    baseProperties.pointBorderWidth = 5;
-    baseProperties.label = ' ';
-  }
-
-  baseProperties.pointBackgroundColor = ['red', 'green', 'blue', 'purple'];
-  baseProperties.label = 'Mentoring';
-
   const numberOfStandards = data[0].scores.length;
   for (let i = 0; i < numberOfStandards; i++) {
     const thisStandardData = data.map(d => d.scores[i]);
     const standardData = Object.assign({}, baseProperties);
     standardData.borderColor = thisStandardData[0].color;
-    // standardData.label = thisStandardData[0].standardName;
-    standardData.data = thisStandardData.map(s => (s.score / 4) * 100);
+    standardData.label = thisStandardData[0].standardName;
+
+    // Change color with mentoring session
+    const isMentoringSessions = data.map(d => d.is_mentoring_session);
     standardData.pointBackgroundColor = [];
+    standardData.pointBorderColor = [];
+    standardData.pointBorderWidth = [];
+    isMentoringSessions.forEach(isMentoringSession => {
+      if (isMentoringSession) {
+        standardData.pointBackgroundColor.push('red');
+        standardData.pointBorderColor.push('red');
+        standardData.pointBorderWidth.push(6);
+      } else {
+        standardData.pointBackgroundColor.push('white');
+        standardData.pointBorderColor.push(thisStandardData[0].color);
+        standardData.pointBorderWidth.push(1);
+      }
+    });
+    //standardData.pointBorderWidth = 5;
+    standardData.data = thisStandardData.map(s => (s.score / 4) * 100);
     formattedData.datasets.push(standardData);
   }
 
@@ -72,5 +74,86 @@ function LineChart({ data } = {}) {
     />
   );
 }
+export default LineChart;
+
+/*
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import { Loader, Message } from 'rsuite';
+
+import styles from './linechart.module.css';
+
+const baseProperties = {
+  fill: false,
+  lineTension: 0.1,
+  pointBackgroundColor: '#fff',
+  pointBorderWidth: 5,
+  label: '',
+  // data, label, borderColor remaining
+};
+
+// TODO clean up this logic
+const formatData = data => {
+  const formattedData = {
+    labels: data.map(d => d.timestamp),
+    datasets: [],
+  };
+
+  console.log('test', data[0]);
+  if (data[0].is_mentoring_session === true) {
+    baseProperties.pointBackgroundColor = 'red';
+    baseProperties.pointBorderWidth = 10;
+  } else {
+    baseProperties.pointBackgroundColor = '#fff';
+    baseProperties.pointBorderWidth = 5;
+    baseProperties.label = ' ';
+  }
+
+  //baseProperties.pointBackgroundColor = ['red', 'green', 'blue', 'purple'];
+
+  //baseProperties.label = 'Mentoring';
+  const numberOfStandards = data[0].scores.length;
+  for (let i = 0; i < numberOfStandards; i++) {
+    const thisStandardData = data.map(d => d.scores[i]);
+    console.log(thisStandardData);
+    const standardData = Object.assign({}, baseProperties);
+    standardData.borderColor = thisStandardData[0].color;
+    standardData.label = thisStandardData[0].standardName;
+    standardData.data = thisStandardData.map(s => (s.score / 4) * 100);
+    standardData.pointBackgroundColor = 'red';
+    formattedData.datasets.push(standardData);
+  }
+
+  console.log(formattedData);
+
+  return formattedData;
+};
+
+function LineChart({ data } = {}) {
+  if (data === null) {
+    return (
+      <Loader className={styles.loading} size="lg" content="Loading data..." />
+    );
+  } else if (data.length) {
+    return (
+      <div>
+        <h2 className={styles.title}>Self-reporting over time</h2>
+        <Line data={formatData(data)} />
+      </div>
+    );
+  }
+
+  return (
+    <Message
+      className={styles.message}
+      type="info"
+      title="No results found"
+      description={
+        <p>Please try setting a broader date range and/or filter.</p>
+      }
+    />
+  );
+}
 
 export default LineChart;
+*/
