d3f468955b0898ed18383dd06c744fe1b395ef0d
Shubham Jain <shubham@sjain.dev>
2021-01-21 19:52:14 +0000

    Format index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Head from 'next/head';
-import Image from 'next/image'
+import Image from 'next/image';
 import { useRef } from 'react';
 
 import { Header } from '../components';
@@ -70,7 +70,6 @@ const errors = {
   },
 };
 
-
 export async function getServerSideProps(context) {
   return { props: { session: await getSession(context) } };
 }
@@ -118,24 +117,26 @@ export default function Home({ session }) {
               Quality Dashboard.
             </p>
             <p>
-              Please expect things to break and bear with us whilst we implement all
-              the features!
+              Please expect things to break and bear with us whilst we implement
+              all the features!
             </p>
             <p>
-              However, if you spot something that doesn't look right please do let
-              us know (via Trello or Whatsapp) and we'll get it fixed!
+              However, if you spot something that doesn't look right please do
+              let us know (via Trello or Whatsapp) and we'll get it fixed!
             </p>
             {/*TODO maybe if unknown user type add an input where they can put their join url? */}
-            {!session && 
+            {!session && (
               <div className="loginButton">
                 <h2> Get started </h2>
                 <Button appearance="primary" onClick={() => signIn('keycloak')}>
                   Login or Register
                 </Button>
               </div>
-            }
+            )}
             <div className="buttonsRow">
-              <Button color="cyan" onClick={() => featuresRef.current.scrollIntoView()}>
+              <Button
+                color="cyan"
+                onClick={() => featuresRef.current.scrollIntoView()}>
                 Features
               </Button>
             </div>
@@ -152,67 +153,66 @@ export default function Home({ session }) {
 
       <div className="features" ref={featuresRef}>
         <div className="feature">
-            <Image
-              src="/images/icons8-todo-list-96.png"
-              width={96}
-              height={96}
-              layout="intrinsic"
-            />
-            <p>
-              Complete your self-reporting on the device of your
-              <br/>
-              choice in a matter of minutes. The self-reporting page
-              <br/>
-              is clear and simple to use allowing you to efficiently
-              <br/>
-              report your recent experience. Remember to submit
-              <br/>
-              as your answers aren't automatically saved.
-            </p>
+          <Image
+            src="/images/icons8-todo-list-96.png"
+            width={96}
+            height={96}
+            layout="intrinsic"
+          />
+          <p>
+            Complete your self-reporting on the device of your
+            <br />
+            choice in a matter of minutes. The self-reporting page
+            <br />
+            is clear and simple to use allowing you to efficiently
+            <br />
+            report your recent experience. Remember to submit
+            <br />
+            as your answers aren't automatically saved.
+          </p>
         </div>
         <div className="feature">
-            <Image
-              src="/images/icons8-combo-chart-96.png"
-              width={96}
-              height={96}
-              layout="intrinsic"
-            />
-            <p>
-              Track your self-reporting any time and on any device.
-              <br/>
-              The statistics page gives you great flexibilty allowing
-              <br/>
-              you to change data ranges and whether the submissions
-              <br/>
-              were a part of a mentoring session. There is also a
-              <br/>
-              quick to read summary at the top which gives you
-              <br/>
-              great insight of your average.
-            </p>
+          <Image
+            src="/images/icons8-combo-chart-96.png"
+            width={96}
+            height={96}
+            layout="intrinsic"
+          />
+          <p>
+            Track your self-reporting any time and on any device.
+            <br />
+            The statistics page gives you great flexibilty allowing
+            <br />
+            you to change data ranges and whether the submissions
+            <br />
+            were a part of a mentoring session. There is also a
+            <br />
+            quick to read summary at the top which gives you
+            <br />
+            great insight of your average.
+          </p>
         </div>
         <div className="feature">
-            <Image
-              src="/images/icons8-people-96.png"
-              width={96}
-              height={96}
-              layout="intrinsic"
-            />
-            <p>
-              Complete your self-reporting by yourself or as part of
-              <br/>
-              a mentoring session. You and your managers can then
-              <br/>
-              use these useful meaningful insights to spark 
-              <br/>
-              conversaions on how you and your department can improve
-              <br/>
-              and what areas are doing well and need to be maintained.
-            </p>
+          <Image
+            src="/images/icons8-people-96.png"
+            width={96}
+            height={96}
+            layout="intrinsic"
+          />
+          <p>
+            Complete your self-reporting by yourself or as part of
+            <br />
+            a mentoring session. You and your managers can then
+            <br />
+            use these useful meaningful insights to spark
+            <br />
+            conversaions on how you and your department can improve
+            <br />
+            and what areas are doing well and need to be maintained.
+          </p>
         </div>
       </div>
       <a href="https://icons8.com">Icons by Icons8</a>
-
 
       <style jsx>{`
         .hero {
@@ -241,35 +241,35 @@ export default function Home({ session }) {
           transform: scale(0) rotate(0deg) translate(-50%, -50%);
           animation: cube 16s ease-in forwards infinite;
         }
-          
+
           .cube:nth-child(2n) {
             border-color: lighten(#0040C1, 10%);
           }
-          
+
           .cube:nth-child(2) {
             animation-delay: 2s;
             left: 25vw;
             top: 40vh;
           }
-          
+
           .cube:nth-child(3) {
             animation-delay: 4s;
             left: 75vw;
             top: 50vh;
           }
-          
+
           .cube:nth-child(4) {
             animation-delay: 6s;
             left: 90vw;
             top: 10vh;
           }
-          
+
           .cube:nth-child(5) {
             animation-delay: 8s;
             left: 10vw;
             top: 85vh;
           }
-          
+
           .cube:nth-child(6) {
             animation-delay: 10s;
             left: 50vw;
@@ -279,15 +279,15 @@ export default function Home({ session }) {
 
         @keyframes cube {
           from {
-            transform: scale(0) rotate(0deg) translate(-50%, -50%);   
+            transform: scale(0) rotate(0deg) translate(-50%, -50%);
             opacity: 1;
           }
           to {
-            transform: scale(20) rotate(960deg) translate(-50%, -50%); 
+            transform: scale(20) rotate(960deg) translate(-50%, -50%);
             opacity: 0;
           }
         }
-        
+
         .features {
           margin: 5vh;
           display: flex;
