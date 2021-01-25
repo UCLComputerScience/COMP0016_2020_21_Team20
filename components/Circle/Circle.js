caa52abbe6ec1f7747da39366ec7c66a62d21e17
Shubham Jain <shubham@sjain.dev>
2021-01-25 23:12:04 +0000

    Use React Suite circles for Circle component
    This removes the need to use SVGs and handle the display logic ourselves
    (and gives us built-in dark theme!).

--- components/Circle/Circle.js
+++ components/Circle/Circle.js
@@ -1,15 +1,11 @@
 import React from 'react';
+import { Progress } from 'rsuite';
+
 import styles from './Circle.module.css';
 
-const circleConfig = {
-  viewBox: '0 0 40 40',
-  x: '20',
-  y: '20',
-  radio: '15.91549430918954',
-};
+const { Circle: CircleComponent } = Progress;
 
 // TODO make this responsive for tablet-sizes and lower (somehow...)
-// Basic implementation from https://dev.to/dastasoft/animated-circle-progress-bar-with-svg-as-react-component-28nm
 const Circle = ({ name, color, percentage }) => {
   return (
     <div className={styles.circle}>
@@ -17,30 +13,7 @@ const Circle = ({ name, color, percentage }) => {
         className={name.length > 10 ? styles.leftOffset : styles.rightOffset}>
         {name}
       </strong>
-      <svg viewBox={circleConfig.viewBox}>
-        <circle
-          cx={circleConfig.x}
-          cy={circleConfig.y}
-          r={circleConfig.radio}
-          fill="transparent"
-          stroke="rgb(220,220,220,0.8)"
-        />
-
-        <circle
-          cx={circleConfig.x}
-          cy={circleConfig.y}
-          r={circleConfig.radio}
-          fill="transparent"
-          stroke={color}
-          strokeDasharray={`${percentage} ${100 - percentage}`}
-          strokeDashoffset={25}
-        />
-        <g className={styles.circleLabel}>
-          <text x="50%" y="50%" className={styles.percentage}>
-            {percentage}%
-          </text>
-        </g>
-      </svg>
+      <CircleComponent percent={percentage} strokeColor={color} />
     </div>
   );
 };
