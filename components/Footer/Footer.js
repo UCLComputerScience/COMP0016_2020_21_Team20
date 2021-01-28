8cb71df69a7331de55c3cb474a1d9af84c4b48dc
Matthieu Schulz <m.schulz18@ejm.org>
2021-01-28 15:55:09 +0000

    new changes implemented after comments

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import { Link } from '@material-ui/core';
+import Link from 'next/link';
 import { Footer as FooterComponent } from 'rsuite';
 import styles from './Footer.module.css';
 
@@ -8,8 +8,10 @@ function Footer() {
       <FooterComponent>
         <div className={styles.About}>
           <p>CQ Dashboard Team 20 UCL</p>
-          <p>UCL Team 20 - IXN Exchange Program</p>
-          <Link>https://comp0016-team-20.github.io/blog/</Link>
+          <p>UCL IXN Exchange Program</p>
+          <Link href="https://comp0016-team-20.github.io/blog/">
+            <a>Blog</a>
+          </Link>
         </div>
       </FooterComponent>
     </div>
