d4a5806d1a80bf98a71e9058775861552396479b
Shubham Jain <shubham@sjain.dev>
2021-02-06 22:08:26 +0000

    Setup swagger-jsdoc; Document GET /api/standards

--- pages/api/standards.js
+++ pages/api/standards.js
@@ -2,6 +2,46 @@ import prisma from '../../lib/prisma';
 
 import requiresAuth from '../../lib/requiresAuthApiMiddleware';
 
+/**
+ * @swagger
+ * tags:
+ *  name: standards
+ *  description: Health & Care Standards
+ */
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    standards:
+ *      properties:
+ *        id:
+ *         type: integer
+ *         example: 1
+ *        name:
+ *          type: string
+ *          example: Timely Care
+ */
+
+/**
+ * @swagger
+ * /standards:
+ *  get:
+ *    summary: Retrieve the list of standards stored in the system
+ *    description: Retrieve the list of standards stored in the system, with their ID and name
+ *    tags: [standards]
+ *    responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/standards'
+ *      401:
+ *        $ref: '#/components/responses/unauthorized'
+ *      500:
+ *        $ref: '#/components/responses/internal_server_error'
+ */
 const handler = async (req, res) => {
   if (req.method === 'GET') {
     const standards = await prisma.standards.findMany();
