bbe4ef666c5d0e547d6aca7b4e9ac98f2629a8d3
Shubham Jain <shubham@sjain.dev>
2021-02-11 17:57:07 +0000

    Document GET /api/recent_words

--- pages/api/recent_words.js
+++ pages/api/recent_words.js
@@ -1,6 +1,35 @@
 import requiresAuth from '../../lib/requiresAuthApiMiddleware';
 import prisma from '../../lib/prisma';
 
+/**
+ * @swagger
+ * tags:
+ *  name: recent_words
+ *  description: Recently Submitted single-words
+ */
+
+/**
+ * @swagger
+ * /recent_words:
+ *  get:
+ *    summary: Retrieve a list of the recently submitted words
+ *    description: Retrieve a list of the most recent 100 words that were submitted by users, anonymised. This can be used for e.g. populating an autocomplete input field
+ *    tags: [recent_words]
+ *    responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: string
+ *                example: tiring
+ *      401:
+ *        $ref: '#/components/responses/unauthorized'
+ *      500:
+ *        $ref: '#/components/responses/internal_server_error'
+ */
 const handler = async (req, res) => {
   // Return a list of recently used words
   if (req.method === 'GET') {
