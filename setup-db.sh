f059f6e6dc3749e1497cd50aeeaa07136cf928f2
Shubham Jain <shubham@sjain.dev>
2021-02-25 23:50:22 +0000

    Update setup-db.sh to use test user credentials for test db
    Also updates the backend:test script to use cross-env to set the
    DATABASE_URL environment variable for testing the backend.

--- setup-db.sh
+++ setup-db.sh
@@ -22,3 +22,12 @@ if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
 fi
 
 cat /docker-entrypoint-initdb.d/schema.txt | psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname care_quality_dashboard
+
+# Create "test" user if we're creating "test" database
+if [[ $POSTGRES_MULTIPLE_DATABASES == *"test"* ]]; then
+echo "Updating test database";
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+		CREATE USER "test" WITH PASSWORD 'test';
+		GRANT ALL PRIVILEGES ON DATABASE test TO "test";
+EOSQL
+fi;
