19bcb57e8aba3848977344a17c2186a42ff24d4f
Shubham Jain <shubham@sjain.dev>
2021-03-04 17:45:40 +0000

    Store seed data in single file

--- test/end-to-end/e2e-test.environment.js
+++ test/end-to-end/e2e-test.environment.js
@@ -7,86 +7,17 @@ const puppeteer = require('puppeteer');
 const fetch = require('node-fetch');
 
 const prisma = require('../../lib/prisma');
+const {
+  standards,
+  likertScaleQuestions,
+  wordsQuestions,
+} = require('../../seedData');
 
 const getClient = async () => {
   const client = new PgClient({ connectionString: process.env.DATABASE_URL });
   await client.connect();
   return client;
 };
-
-const standards = [
-  'Staff and Resources',
-  'Staying Healthy',
-  'Individual Care',
-  'Timely Care',
-  'Dignified Care',
-  'Effective Care',
-  'Safe Care',
-  'Governance, Leadership and Accountability',
-];
-
-const likertScaleQuestions = [
-  {
-    question:
-      'I am confident/reassured that I have screened for serious pathology to an appropriate level in this case.',
-    standardId: 1,
-    url:
-      'http://www.wales.nhs.uk/governance-emanual/theme-7-staff-and-resources',
-  },
-  {
-    question:
-      'I have applied knowledge of best evidence to the context of this patient’s presentation to present appropriate treatment options to the patient.',
-    standardId: 2,
-    url: 'http://www.wales.nhs.uk/governance-emanual/theme-1-staying-healthy',
-  },
-  {
-    question:
-      'I have optimised the opportunity in our interaction today to discuss relevant activities and behaviours that support wellbeing and a healthy lifestyle for this patient.',
-    standardId: 3,
-    url: 'http://www.wales.nhs.uk/governance-emanual/theme-6-individual-care',
-  },
-  {
-    question:
-      'I have listened and responded with empathy to the patient’s concerns.',
-    standardId: 4,
-    url: 'http://www.wales.nhs.uk/governance-emanual/theme-5-timely-care',
-  },
-  {
-    question:
-      'I have supported the patient with a shared decision making process to enable us to agree a management approach that is informed by what matters to them.',
-    standardId: 5,
-    url: 'http://www.wales.nhs.uk/governance-emanual/theme-4-dignified-care',
-  },
-  {
-    question:
-      'I have established progress markers to help me and the patient monitor and evaluate the success of the treatment plan.',
-    standardId: 6,
-    url: 'http://www.wales.nhs.uk/governance-emanual/theme-3-effective-care',
-  },
-  {
-    question:
-      'My reflection/discussion about this interaction has supported my development through consolidation or a unique experience I can learn from.',
-    standardId: 7,
-    url: 'http://www.wales.nhs.uk/governance-emanual/theme-2-safe-care',
-  },
-];
-
-const wordsQuestions = [
-  {
-    question:
-      'Provide 3 words that describe enablers/facilitators to providing high quality effective care in this interaction.',
-    standardId: 8,
-    url:
-      'http://www.wales.nhs.uk/governance-emanual/governance-leadership-and-accountability-1',
-  },
-  {
-    question:
-      'Provide 3 words that describe barriers/challenges to providing high quality effective care in this interaction.',
-    standardId: 8,
-    url:
-      'http://www.wales.nhs.uk/governance-emanual/governance-leadership-and-accountability-1',
-  },
-];
 
 const users = [
   {
