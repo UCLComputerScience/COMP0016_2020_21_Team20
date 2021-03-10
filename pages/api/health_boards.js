13f09f9ae17f6abf293d3febe73d3dc27175fa9d
Shubham Jain <shubham@sjain.dev>
2021-03-10 11:59:13 +0000

    Add POST /api/health_boards endpoint for admins

--- pages/api/health_boards.js
+++ pages/api/health_boards.js
@@ -45,6 +45,42 @@ import { Roles } from '../../lib/constants';
  *        $ref: '#/components/responses/insufficient_permission'
  *      500:
  *        $ref: '#/components/responses/internal_server_error'
+ *  post:
+ *    summary: Add a new health board
+ *    description: "Add a new health board to the system. Note: you must be an administrator to be able to perform this operation."
+ *    tags: [health_boards]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *                example: Aneurin Bevan
+ *    responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/health_board'
+ *      401:
+ *        $ref: '#/components/responses/unauthorized'
+ *      403:
+ *        $ref: '#/components/responses/insufficient_permission'
+ *      422:
+ *        description: Invalid details
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/error'
+ *            example:
+ *              error: true
+ *              message: The required hospital details are missing
+ *      500:
+ *        $ref: '#/components/responses/internal_server_error'
  */
 const handler = async (req, res) => {
   const { session } = req;
@@ -64,6 +100,28 @@ const handler = async (req, res) => {
 
     // Return the name and id of the health boards
     return res.json(healthBoards);
+  }
+
+  if (req.method === 'POST') {
+    if (!session.user.roles.includes(Roles.USER_TYPE_ADMIN)) {
+      return res.status(403).json({
+        error: true,
+        message:
+          'You do not have permission to add health boards to the system',
+      });
+    }
+
+    const { name } = req.body;
+    if (!name) {
+      return res.status(422).json({
+        error: true,
+        message: 'The required health board details are missing',
+      });
+    }
+
+    const record = await prisma.health_boards.create({ data: { name } });
+
+    return res.json(record);
   }
 
   res.status(405).json({ error: true, message: 'Method Not Allowed' });
--- test/api/health_boards.test.js
+++ test/api/health_boards.test.js
@@ -76,3 +76,96 @@ describe('GET /api/health_boards', () => {
     });
   });
 });
+
+describe('POST /api/health_boards', () => {
+  it('is guarded by auth', async () => {
+    expect.hasAssertions();
+    helpers.mockSessionWithUserType(null);
+    await testApiHandler({
+      handler,
+      test: async ({ fetch }) => {
+        const res = await fetch();
+        expect(res.status).toBe(401);
+      },
+    });
+  });
+
+  [
+    Roles.USER_TYPE_CLINICIAN,
+    Roles.USER_TYPE_DEPARTMENT,
+    Roles.USER_TYPE_HOSPITAL,
+    Roles.USER_TYPE_HEALTH_BOARD,
+    Roles.USER_TYPE_UNKNOWN,
+  ].forEach(userType => {
+    it(`doesn't allow ${userType} user types to add health boards`, async () => {
+      expect.hasAssertions();
+      helpers.mockSessionWithUserType(userType);
+      await testApiHandler({
+        handler,
+        test: async ({ fetch }) => {
+          const res = await fetch({
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: '',
+          });
+          expect(res.status).toBe(403);
+
+          const json = await res.json();
+          const validator = await helpers.getOpenApiValidatorForRequest(
+            '/health_boards',
+            'post'
+          );
+          expect(validator.validateResponse(403, json)).toEqual(undefined);
+        },
+      });
+    });
+  });
+
+  it('allows admins to add a new health board', async () => {
+    expect.hasAssertions();
+    helpers.mockSessionWithUserType(Roles.USER_TYPE_ADMIN);
+    await testApiHandler({
+      handler,
+      test: async ({ fetch }) => {
+        const res = await fetch({
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            name: 'Test Health Board 100',
+          }),
+        });
+        expect(res.status).toBe(200);
+
+        const json = await res.json();
+        const validator = await helpers.getOpenApiValidatorForRequest(
+          '/health_boards',
+          'post'
+        );
+        expect(validator.validateResponse(200, json)).toEqual(undefined);
+      },
+    });
+  });
+
+  it('validates request bodies', async () => {
+    expect.hasAssertions();
+    helpers.mockSessionWithUserType(Roles.USER_TYPE_ADMIN);
+    await testApiHandler({
+      handler,
+      test: async ({ fetch }) => {
+        const res = await fetch({
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ foo: 'bar' }),
+        });
+        expect(res.status).toBe(422);
+
+        const json = await res.json();
+        const validator = await helpers.getOpenApiValidatorForRequest(
+          '/health_boards',
+          'post'
+        );
+        expect(validator.validateResponse(422, json)).toEqual(undefined);
+      },
+    });
+  });
+});
