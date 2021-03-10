dd675c460bbfd229d575ba5067e077bde4615421
Shubham Jain <shubham@sjain.dev>
2021-03-10 11:10:11 +0000

    Allow admins to fetch hospitals from GET /api/hospitals

--- pages/api/hospitals.js
+++ pages/api/hospitals.js
@@ -21,6 +21,15 @@ import { Roles } from '../../lib/constants';
  *        name:
  *          type: string
  *          example: The Grange University Hospital
+ *        health_board
+ *          type: object
+ *          properties:
+ *            id:
+ *              type: integer
+ *              example: 1
+ *            name:
+ *              type: string
+ *              example: Aneurin Bevan
  */
 
 /**
@@ -28,7 +37,7 @@ import { Roles } from '../../lib/constants';
  * /hospitals:
  *  get:
  *    summary: Retrieve a list of hospitals
- *    description: "Retrieve a list of hospitals. Note: only health board users can use this endpoint to fetch hospitals inside their own health board. This could be used e.g. for populating a list of hospitals for them to filter by when visualising statistics."
+ *    description: "Retrieve a list of hospitals. Note: only health board users can use this endpoint to fetch hospitals inside their own health board, or platform administrators to view all hospitals in the system. This could be used e.g. for populating a list of hospitals for them to filter by when visualising statistics."
  *    tags: [hospitals]
  *    responses:
  *      200:
@@ -50,22 +59,40 @@ const handler = async (req, res) => {
   const { session } = req;
 
   if (req.method === 'GET') {
-    if (!session.user.roles.includes(Roles.USER_TYPE_HEALTH_BOARD)) {
+    if (
+      !session.user.roles.includes(Roles.USER_TYPE_HEALTH_BOARD) &&
+      !session.user.roles.includes(Roles.USER_TYPE_ADMIN)
+    ) {
       return res.status(403).json({
         error: true,
         message: 'You do not have permission to view hospitals',
       });
     }
 
+    let where = {};
+    if (session.user.roles.includes(Roles.USER_TYPE_HEALTH_BOARD)) {
+      where = { health_board_id: { equals: session.user.healthBoardId } };
+    }
+
     const hospitals = await prisma.hospitals.findMany({
-      where: {
-        health_board_id: { equals: session.user.healthBoardId },
+      where,
+      select: {
+        health_boards: { select: { name: true, id: true } },
+        id: true,
+        name: true,
       },
+      orderBy: { id: 'asc' },
     });
 
-    // Only return the name and id of the hospital
+    // Return the name, id and health board of the hospital
     // TODO return as object with key `hospitals` and value the array:
-    return res.json(hospitals.map(h => ({ name: h.name, id: h.id })));
+    return res.json(
+      hospitals.map(h => ({
+        name: h.name,
+        id: h.id,
+        health_board: h.health_boards,
+      }))
+    );
   }
 
   res.status(405).json({ error: true, message: 'Method Not Allowed' });
--- test/api/hospitals.test.js
+++ test/api/hospitals.test.js
@@ -30,7 +30,6 @@ describe('GET /api/hospitals', () => {
     Roles.USER_TYPE_CLINICIAN,
     Roles.USER_TYPE_DEPARTMENT,
     Roles.USER_TYPE_HOSPITAL,
-    Roles.USER_TYPE_ADMIN,
     Roles.USER_TYPE_UNKNOWN,
   ].forEach(userType => {
     it(`rejects ${userType}s`, async () => {
@@ -46,24 +45,53 @@ describe('GET /api/hospitals', () => {
     });
   });
 
-  it('returns hospitals', async () => {
-    expect.hasAssertions();
-    helpers.mockSessionWithUserType(Roles.USER_TYPE_HEALTH_BOARD);
-    await testApiHandler({
-      handler,
-      test: async ({ fetch }) => {
-        const res = await fetch();
-        expect(res.status).toBe(200);
+  [Roles.USER_TYPE_ADMIN, Roles.USER_TYPE_HEALTH_BOARD].forEach(userType => {
+    it(`returns hospitals to ${userType} users`, async () => {
+      expect.hasAssertions();
+      helpers.mockSessionWithUserType(userType);
+      await testApiHandler({
+        handler,
+        test: async ({ fetch }) => {
+          const res = await fetch();
+          expect(res.status).toBe(200);
 
-        const json = await res.json();
-        const validator = await helpers.getOpenApiValidatorForRequest(
-          '/hospitals'
-        );
-        expect(validator.validateResponse(200, json)).toEqual(undefined);
-        expect(json.length).toEqual(2);
-        expect(json[0]).toEqual({ name: 'Test Hospital', id: 1 });
-        expect(json[1]).toEqual({ name: 'Test Hospital 2', id: 2 });
-      },
+          const json = await res.json();
+          const validator = await helpers.getOpenApiValidatorForRequest(
+            '/hospitals'
+          );
+
+          expect(validator.validateResponse(200, json)).toEqual(undefined);
+          expect(json.length).toEqual(
+            userType === Roles.USER_TYPE_ADMIN ? 4 : 2
+          );
+
+          expect(json[0]).toMatchObject({
+            name: 'Test Hospital',
+            id: 1,
+            health_board: { id: 1, name: 'Test Health Board' },
+          });
+
+          expect(json[1]).toMatchObject({
+            name: 'Test Hospital 2',
+            id: 2,
+            health_board: { id: 1, name: 'Test Health Board' },
+          });
+
+          if (userType === Roles.USER_TYPE_ADMIN) {
+            expect(json[2]).toMatchObject({
+              name: 'Test Hospital 3',
+              id: 3,
+              health_board: { id: 2, name: 'Test Health Board 2' },
+            });
+
+            expect(json[3]).toMatchObject({
+              name: 'Test Hospital 4',
+              id: 4,
+              health_board: { id: 2, name: 'Test Health Board 2' },
+            });
+          }
+        },
+      });
     });
   });
 });
